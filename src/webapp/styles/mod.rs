764fe24881aa97964185a113dcf2f66bc6ae1766
Jeffery Olson <olson.jeffery@gmail.com>
2026-07-06 18:03:42 -0500

    add app and bulmaswatch side-by-side

--- src/webapp/styles/mod.rs
+++ src/webapp/styles/mod.rs
@@ -1,1 +1,14 @@
+pub mod app {
+    use leptos_styling::style_sheet;
 
+    style_sheet!(app_styles, "src/webapp/styles/app.css", "app_styles");
+}
+pub mod bulmaswatch {
+    use leptos_styling::style_sheet;
+
+    style_sheet!(
+        bulma_styles,
+        "src/webapp/styles/bulmaswatch.min.css",
+        "bulma_styles"
+    );
+}
